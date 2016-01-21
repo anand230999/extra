47d33cd11edab21336720545295d3e15261025ac
Anand Krishnan <anand230999@gmail.com>
2016-01-21 14:46:52 +0530

    Create chat.js

---
+++ simple-chat-server-using-node-js/chat.js
@@ -0,0 +1,16 @@
+var http = require('http'),
+    fs = require('fs');
+var app = http.createServer(function (request, response) {
+    fs.readFile("chat.html", 'utf-8', function (error, data) {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
+    });
+}).listen(8000);
+var io = require('socket.io').listen(app);
+io.sockets.on('connection', function(socket) {
+    var ip = socket.handshake.address;
+    socket.on('message_to_server', function(data) {
+        io.sockets.emit("message_to_client",{ message: ip+" : "+data["message"] });
+    });
+});
